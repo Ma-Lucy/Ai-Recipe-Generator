915a8a7dac512374c44a45eadfd219345739bc23
Ma-Lucy <161073044+Ma-Lucy@users.noreply.github.com>
2024-04-19 11:26:41 +0100

    Imported typewriter plug in
    Temporarily used it in the  handle submit function

--- src/script.js
+++ src/script.js
@@ -1,8 +1,17 @@
 function handleSubmit(event) {
   event.preventDefault();
   let recipeBox = document.querySelector("#recipe");
-  recipeBox.innerHTML = "Generating a recipe for you";
+
+  new Typewriter("#recipe", {
+    strings: "Generating a recipe for you",
+    autoStart: true,
+    cursor: "",
+    delay: 15,
+  });
 }
 
 let submitButton = document.querySelector("#recipe-form");
 submitButton.addEventListener("submit", handleSubmit);
+
+//add api to event function - include api ket prompt etc
+//create another function to handle response, with typewriter plugin
