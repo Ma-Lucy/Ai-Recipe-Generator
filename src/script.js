a5f8ec2df9490df559c4daa3120ca6ca2269f179
Ma-Lucy <161073044+Ma-Lucy@users.noreply.github.com>
2024-04-19 14:20:13 +0100

    imported axios, and shecodes AI API
    created JS functions using the API and to generate a response using the typewriter plug in
    
    Also deleted the example data

--- src/script.js
+++ src/script.js
@@ -1,17 +1,27 @@
-function handleSubmit(event) {
-  event.preventDefault();
+function recipeFunction(response) {
   let recipeBox = document.querySelector("#recipe");
+  recipeBox.innerHTML = "Generating a recipe for you... please wait";
 
   new Typewriter("#recipe", {
-    strings: "Generating a recipe for you",
+    strings: response.data.answer,
     autoStart: true,
+    delay: 20,
     cursor: "",
-    delay: 15,
   });
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let inputElement = document.querySelector("#input");
+  let apiKey = "6778eb30a3f8f38t710a50259f4eoc6f";
+  let prompt = `Please use ${inputElement.value} to create a simple meal recipe. List the ingredients first followed by the instructions`;
+  let context =
+    "You are an experieced chef and know how to use up left over ingredients creatively, please answer in basic html format";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(recipeFunction);
 }
 
 let submitButton = document.querySelector("#recipe-form");
 submitButton.addEventListener("submit", handleSubmit);
-
-//add api to event function - include api ket prompt etc
-//create another function to handle response, with typewriter plugin
